e73b9a2e6a1cbfa08deca223fe658326b1170f9f
Ankit150595 <ankit150595@gmail.com>
2021-08-27 13:20:11 +0530

    modified

--- src/Components/HotelFilterPage/HotelFilterCard.jsx
+++ src/Components/HotelFilterPage/HotelFilterCard.jsx
@@ -7,7 +7,7 @@ export default function HotelFilterCard() {
         <div className={styles.hotel_card_container}>
             <div className={styles.hotel_card_left}>
                 <img src="https://imgcld.yatra.com/ytimages/image/upload/t_hotel_yatra_details_desktop/v1509531805/Hotel/00096502/Executive_Room_4kPV3N.jpg" alt="" />
-                <div><span>#Friends getaways &nbsp;&nbsp;#Family</span><span><SearchIcon/></span></div>
+                <div><span>#Friends getaways &nbsp;&nbsp;#Family</span><span><SearchIcon /></span></div>
             </div>
             <div className={styles.hotel_card_middle}></div>
             <div className={styles.hotel_card_right}></div>
