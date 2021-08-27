5e2c63ebea3b0348ef0f02d2d2bd1b85010ce4bc
Bharadwaj <sbhatbharadwaj24@gmail.com>
2021-08-27 12:02:35 +0530

    minor css changes

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,18 +1,18 @@
-import { SearchBanner } from "./SearchBanner"
-import { BestSelling } from "./BestSelling"
-import {TrendingDestinations} from './TrendingDestinations'
-import { CustomerReviews } from './CustomerReviews'
-import MainFooter from './MainFooter'
-
+import { SearchBanner } from "./SearchBanner";
+import { BestSelling } from "./BestSelling";
+import { TrendingDestinations } from "./TrendingDestinations";
+import { CustomerReviews } from "./CustomerReviews";
+import MainFooter from "./MainFooter";
+import styles from "../../Styles/landingPage.module.css";
 
 export const LandingPage = () => {
-    return (
-        <div>
-            <SearchBanner />
-            <BestSelling />
-            <TrendingDestinations />
-            <CustomerReviews />
-            <MainFooter/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <SearchBanner />
+      <BestSelling />
+      <TrendingDestinations />
+      <CustomerReviews />
+      <MainFooter />
+    </div>
+  );
+};
