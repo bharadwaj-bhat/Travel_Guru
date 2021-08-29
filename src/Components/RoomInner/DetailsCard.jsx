602a4c970123b667695b10fca80d3590efbd2eff
Bharadwaj <sbhatbharadwaj24@gmail.com>
2021-08-29 15:05:37 +0530

    loading spinner

--- src/Components/RoomInner/DetailsCard.jsx
+++ src/Components/RoomInner/DetailsCard.jsx
@@ -4,6 +4,7 @@ import { getData } from "../../redux/RoomInner/actions";
 import { Carousel, CarouselComp } from "./Carousel";
 import { NameHeader } from "./NameHeader";
 import { NavigationBar } from "./NavigationBar";
+import Loader from "react-loader-spinner";
 
 export const DetailsCard = ({ id }) => {
   const data = useSelector((state) => state.room.data);
@@ -15,7 +16,27 @@ export const DetailsCard = ({ id }) => {
   }, []);
 
   if (loading) {
-    return <h1> Loading...</h1>;
+    return (
+      <div
+        style={{
+          margin: "150px auto",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingBottom: "200px",
+        }}
+      >
+        <Loader
+          type="Circles"
+          color="#F47932"
+          height={100}
+          width={100}
+          timeout={4000} //3 secs
+        />
+        <p>Loading... thanks for waiting</p>
+      </div>
+    );
   }
 
   return (
--- src/Components/aTest/Testing.jsx
+++ src/Components/aTest/Testing.jsx
@@ -26,14 +26,14 @@ const password = "mxfzlfR60GuQjY69sl91JqOb";
 const token = Buffer.from(`${username}:${password}`, "utf8").toString("base64");
 export const Testing = () => {
   useEffect(() => {
-    axios
-      .post("https://api.razorpay.com/v1/orders", data, {
-        headers: {
-          Authorization: `Basic ${token}`,
-        },
-      })
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
+    // axios
+    //   .post("https://api.razorpay.com/v1/orders", data, {
+    //     headers: {
+    //       Authorization: `Basic ${token}`,
+    //     },
+    //   })
+    //   .then((res) => console.log(res))
+    //   .catch((err) => console.error(err));
   }, []);
 
   async function displayRazorpay() {
@@ -45,13 +45,13 @@ export const Testing = () => {
     }
     const options = {
       key: "rzp_test_k8s6vfpDLGVjUZ",
-      amount: "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      amount: "70000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
       currency: "INR",
       name: "Travel Guru",
       description: "Thank you",
       image:
         "https://www.travelguru.com/travelguru/resources/beetle/images/tg/travelguru-homestay-logo-199x52.png",
-      order_id: "order_HqlYVy8A7Cnxcz", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      order_id: "order_HqwYVpWN61KC15", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
       handler: function (response) {
         alert(response.razorpay_payment_id);
         // alert(response.razorpay_order_id);
